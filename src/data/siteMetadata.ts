da4f8ea704b7adfd508dd6a37c68c6f51bb34854
ImLJS <109210868+ImLJS@users.noreply.github.com>
2025-06-08 20:23:01 +0530

    [Feat] - Add SIteMetadata and skills

---
+++ src/data/siteMetadata.ts
@@ -0,0 +1,17 @@
+export const siteMetadata = {
+	title: "me",
+	name: "Leone Jacob",
+	headerTitle: "Leone Jacob",
+	github: process.env.NEXT_PUBLIC_GITHUB,
+	linkedin: process.env.NEXT_PUBLIC_LINKEDIN,
+	email: process.env.NEXT_PUBLIC_EMAIL,
+	spotifyClientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+	spotifyClientSecret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
+	spotifyRefreshToken: process.env.NEXT_PUBLIC_SPOTIFY_REFRESH_TOKEN,
+	homeDescription:
+		"Software engineer building products to help people lead better lives 🎯",
+	aboutDescription: `I'm a Software Engineer not afraid of any technology 🎓`,
+	projectsDescription:
+		"I love shipping products and building great software 🔨",
+	githubUser: process.env.NEXT_PUBLIC_GITHUB_USERNAME,
+};
