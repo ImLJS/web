cd7b7dac611a16a8858b510901d271017bb7d824
ImLJS <109210868+ImLJS@users.noreply.github.com>
2025-07-06 12:55:26 +0530

    [Feat] - Add Github Oauth

--- src/env.js
+++ src/env.js
@@ -13,6 +13,8 @@ export const env = createEnv({
 		SPOTIFY_REFRESH_TOKEN: z.string(),
 		BETTER_AUTH_SECRET: z.string(),
 		BETTER_AUTH_URL: z.string().url(),
+		GITHUB_CLIENT_ID: z.string(),
+		GITHUB_CLIENT_SECRET: z.string(),
 		NODE_ENV: z
 			.enum(["development", "test", "production"])
 			.default("development"),
@@ -43,6 +45,8 @@ export const env = createEnv({
 		SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
 		SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
 		SPOTIFY_REFRESH_TOKEN: process.env.SPOTIFY_REFRESH_TOKEN,
+		GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+		GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
 		BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
 		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
 		NODE_ENV: process.env.NODE_ENV,
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,17 @@
+import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
- 
+
 const prisma = new PrismaClient();
- 
+
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: "sqlite",
-  }),
+	database: prismaAdapter(prisma, {
+		provider: "sqlite",
+	}),
+	socialProviders: {
+		github: {
+			clientId: process.env.GITHUB_CLIENT_ID as string,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+		},
+	},
 });
