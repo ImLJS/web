02af9dc318ff211e39cd954daecc005b4bfc866f
ImLJS <109210868+ImLJS@users.noreply.github.com>
2025-07-17 15:08:36 +0530

    [Feat] - Add Basic Gallery Form

---
+++ src/app/(admin)/admin/gallery/page.tsx
@@ -0,0 +1,11 @@
+import GalleryForm from "@/components/admin/gallery/gallery-form";
+
+const GalleryPage = () => {
+	return (
+		<div className="mt-6 space-y-10 md:mt-10 md:space-y-16">
+			<GalleryForm />
+		</div>
+	);
+};
+
+export default GalleryPage;
