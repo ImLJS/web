eac2904694ec6bca9f2b43e9c9b6c316028da416
ImLJS <109210868+ImLJS@users.noreply.github.com>
2025-07-20 11:35:26 +0530

    [Feat] - Add Row Selection

--- src/components/admin/gallery/columns.tsx
+++ src/components/admin/gallery/columns.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Checkbox } from "@/components/ui/checkbox";
 import { DataTableColumnHeader } from "@/components/ui/data-table/data-table-column-header";
 import type { ColumnDef } from "@tanstack/react-table";
 import { z } from "zod";
@@ -16,6 +17,34 @@ export const gallerySchema = z.object({
 export type Gallery = z.infer<typeof gallerySchema>;
 
 export const columns: ColumnDef<Gallery>[] = [
+	{
+		id: "select",
+		header: ({ table }) => (
+			<div className="flex items-center justify-center">
+				<Checkbox
+					checked={
+						table.getIsAllRowsSelected() ||
+						(table.getIsSomeRowsSelected() && "indeterminate")
+					}
+					onCheckedChange={(value) => table.toggleAllRowsSelected(!!value)}
+					aria-label="Select all"
+					className="translate-y-[2px]"
+				/>
+			</div>
+		),
+		cell: ({ row }) => (
+			<div className="flex items-center justify-center">
+				<Checkbox
+					checked={row.getIsSelected()}
+					onCheckedChange={(value) => row.toggleSelected(!!value)}
+					aria-label="Select row"
+					className="translate-y-[2px]"
+				/>
+			</div>
+		),
+		enableSorting: false,
+		enableHiding: false,
+	},
 	{
 		accessorKey: "id",
 		accessorFn: (row) => row?.id?.toString(),
