1cb712ea0a46225b13b4bb8c5a08941f63eed8b7
ImLJS <109210868+ImLJS@users.noreply.github.com>
2025-11-14 09:22:44 +0530

    chore(refactor): remove all files

--- src/app/api/auth/[...all]/route.ts
+++
@@ -1,4 +0,0 @@
-import { handler } from "@/server/auth";
-import { toNextJsHandler } from "better-auth/next-js";
-
-export const { POST, GET } = toNextJsHandler(handler);
--- src/app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-import ErrorPage from "@/components/shared/error-page";
-
-const NotFound = () => {
-	return <ErrorPage type="404" />;
-};
-
-export default NotFound;
