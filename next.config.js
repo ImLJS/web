61f386995d1dbaa34fd8ccef186c831cd9b0256d
ImLJS <109210868+ImLJS@users.noreply.github.com>
2025-12-04 06:39:22 +0530

    feat: react compiler

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
 	reactStrictMode: true,
+	reactCompiler: true,
 	transpilePackages: ["geist"],
 	images: {
 		remotePatterns: [
